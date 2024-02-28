12db9aa9c53fa22935659ab8aa890bb8df63e350
Claudinei José Santos <claudineibitta@gmail.com>
2024-02-28 20:43:04 -0300

    feat: interface e repository

---
+++ Repository/IRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace poo_lab_dio.Repository
+{
+    public interface IRepository<T> where T : class
+    {
+        List<T> Lista();
+        T RetornaPorId(int id);
+        void Insere(T entidade);
+        void Exclui(int id);
+        void Atualiza(int id, T entidade);
+        int ProximoId();
+    }
+}
