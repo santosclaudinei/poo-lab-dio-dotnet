e06fe8bed2e6d15f8d127b3eae347b71f6bca74f
Claudinei José Santos <claudineibitta@gmail.com>
2024-02-28 20:42:25 -0300

    feat: criacao de models

---
+++ Models/IEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace poo_lab_dio.Models
+{
+    public abstract class IEntity
+    {
+        public long Id { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime UpdatedAt { get; set; }
+    }
+}
---
+++ Models/Serie.cs
@@ -0,0 +1,51 @@
+﻿namespace poo_lab_dio.Models
+{
+    public class Serie : IEntity
+    {
+        public Serie(int id, Genero genero, string titulo, string descricao, int ano)
+        {
+            this.Id = id;
+            this.Genero = genero;
+            this.Titulo = titulo;
+            this.Descricao = descricao;
+            this.Ano = ano;
+            this.Excluido = false;
+        }
+
+        public Genero Genero { get; set; }
+        public string? Titulo { get; set; }
+        public string? Descricao { get; set; }
+        public int Ano { get; set; }
+        private bool Excluido { get; set; }
+
+        public override string ToString()
+        {
+            // Environment.NewLine https://docs.microsoft.com/en-us/dotnet/api/system.environment.newline?view=netcore-3.1
+            string retorno = "";
+            retorno += "Gênero: " + this.Genero + Environment.NewLine;
+            retorno += "Titulo: " + this.Titulo + Environment.NewLine;
+            retorno += "Descrição: " + this.Descricao + Environment.NewLine;
+            retorno += "Ano de Início: " + this.Ano + Environment.NewLine;
+            retorno += "Excluido: " + this.Excluido;
+            return retorno;
+        }
+
+        public string? retornaTitulo()
+        {
+            return this.Titulo;
+        }
+
+        public long retornaId()
+        {
+            return this.Id;
+        }
+        public bool retornaExcluido()
+        {
+            return this.Excluido;
+        }
+        public void Excluir()
+        {
+            this.Excluido = true;
+        }
+    }
+}
